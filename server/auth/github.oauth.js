7962dada732884cda078141e95bf0c0721157fea
“Joshua <j.maas.howard@gmail.com>
2016-08-31 18:04:49 -0400

    try us, betches

---
+++ server/auth/github.oauth.js
@@ -0,0 +1,39 @@
+'use strict';
+
+var router = require('express').Router();
+var passport = require('passport');
+var GitHubStrategy = require('passport-github').Strategy;
+
+var notSecrets = require('./dontlookhere');
+
+var User = require('../api/users/user.model');
+
+router.get('/', passport.authenticate('github'));
+
+router.get('/callback', passport.authenticate('github', {
+  successRedirect: '/stories',
+  failureRedirect: '/signup'
+}));
+
+passport.use(new GitHubStrategy({
+  clientID: notSecrets.git.ID,
+  clientSecret: notSecrets.git.Secret,
+  callbackURL: notSecrets.git.URL
+}, function (token, refreshToken, profile, done) {
+  var info = {
+    name: profile.displayName,
+    // github may not provide an email, if so we'll just fake it
+    email: profile.emails ? profile.emails[0].value : [profile.username , 'fake-auther-email.com'].join('@'),
+    photo: profile.photos ? profile.photos[0].value : undefined
+  };
+  User.findOrCreate({
+    where: {githubId: profile.id},
+    defaults: info
+  })
+  .spread(function (user) {
+    done(null, user);
+  })
+  .catch(done);
+}));
+
+module.exports = router;
